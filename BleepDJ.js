e2c6d44afc57da666a15c1e8882b9584d046cde6
Brian Arnold <brianarn@gmail.com>
2010-05-01 19:19:13 -0600

    Trying to scaffold up

---
+++ BleepDJ.js
@@ -0,0 +1,20 @@
+/*
+ * BleepDJ
+ * My first Dojo project/dijit, also a hopefully amusing audio experiment
+ * Copyright Brian Arnold, 2010
+ *
+ * MIT Licensed, so, y'know, do what'cha want with it
+ * See LICENSE for details
+ *
+ * Any audio files associated with this project are licensed under
+ * Creative Commons Attribution 3.0 United States License
+ * See http://creativecommons.org/licenses/by/3.0/us/ for details
+ */
+ 
+ // Require our Dijit
+ dojo.require('rtnet/BleepDJ');
+ 
+ // On DOM load, drop one in the page
+ dojo.ready(function(){
+     
+ }); // dojo.ready
