7a62f244cfcd5ad0938ef0ecd54b940113b30d4f
Brian Arnold <brianarn@gmail.com>
2010-05-02 12:54:23 -0600

    A first pass at some controls, some interaction

--- rtnet/BleepDJ.js
+++ rtnet/BleepDJ.js
@@ -17,6 +17,8 @@ dojo.provide('rtnet.BleepDJ');
 // Bring in template-y stuff
 dojo.require('dijit._Widget');
 dojo.require('dijit._Templated');
+dojo.require('dijit.form.Button');
+dojo.require('dijit.form.Slider');
 
 // Build 'er up
 dojo.declare(
@@ -24,32 +26,42 @@ dojo.declare(
     [dijit._Widget, dijit._Templated],
     {
         templatePath: dojo.moduleUrl('rtnet', 'templates/BleepDJ.html'),
+        widgetsInTemplate: true,
 
         // Local variables - the note array is intentionally defined here for all
         notes: ['c2','d2','e2','f2','g2','a2','b2','c3'],
         cols: null, // Will be a set of query objects for LEDs per column
         curCol: 0,
         numCols: 0,
-        curTempo: 120,
         isPlaying: false,
 
-        postCreate: function() {
+        postCreate: function () {
             var ledCols, i, l;
             
-            ledCols = dojo.query('.bleepdj_led_col');
+            // Build up an array of NodeLists of LEDs per column
+            ledCols = dojo.query('.bleepdj_led_col', this.domNode);
             this.cols = [];
             this.numCols = ledCols.length;
             for (i = 0, l = ledCols.length; i < l; i++) {
                 this.cols.push(dojo.query('.bleepdj_led', ledCols[i]));
             }
-
-            /* Cycle the lights */
-            this.cols[this.curCol].addClass('on');
-            setInterval(dojo.hitch(this, function(){
+            
+            dojo.query('.bleepdj_led', this.domNode).onclick(function (e){
+                dojo.toggleClass(e.target, 'on');
+            });
+            
+            // Cycle the lights
+            this.curCol = this.numCols - 1;
+            (dojo.hitch(this, function cycle(){
                 this.cols[this.curCol].removeClass('on');
                 this.curCol = (this.curCol + 1) % this.numCols;
                 this.cols[this.curCol].addClass('on');
-            }), 1000 / this.curTempo * 60);
+                setTimeout(dojo.hitch(this, cycle), 1000 / this.tempo.value * 30);
+            }))();
+        }, 
+        
+        _playClick: function (e) {
+            console.log('_playClick');
         }
     }
 ); // declare('rtnet.BleepDJ')
